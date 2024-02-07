2bd6a3a9093a4e49e9b158be725469966e535aaf
sabbirsun <sabbirsun1920@gmail.com>
2024-02-07 21:30:30 +0600

    state

--- practice/src/Pages/About.jsx
+++ practice/src/Pages/About.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
 export default class About extends Component {
+    state ={
+        name:"Sabbir",
+        age:24,
+        Prof:"Programmer"
+    }
   render() {
     return (
       <div>
-        <h3>About</h3>
+        <h3>{"My name is " + this.state.name + " & My Profession is " + this.state.Prof}</h3>
       </div>
     )
   }
