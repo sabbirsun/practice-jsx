8a8a084abd85c74debe679a9dd6d9ae118f6837b
sabbirsun <sabbirsun1920@gmail.com>
2024-02-08 20:40:28 +0600

    module css

--- practice/src/Pages/Style.jsx
+++ practice/src/Pages/Style.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Style.css"
+import SunStyle from "./Sun.module.css"
 
 function Style() {
     const Element={
@@ -11,6 +12,7 @@ function Style() {
     <div>
         <h1 style={Element}>My First Style Component</h1>
         <h4 className='Sun'>My name is Md. Sabbirul Islam Sun</h4>
+        <h4 className={SunStyle.Sun}>My name is Md. Sabbirul Islam Sun</h4>
     </div>
   )
 }
