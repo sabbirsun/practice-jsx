a61a204c9536f746a5f3146cd516e1da3ea25f1d
sabbirsun <sabbirsun1920@gmail.com>
2024-02-08 21:28:14 +0600

    New value added

--- practice/src/Pages/Input.jsx
+++ practice/src/Pages/Input.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 function Input() {
     const FormChange = (e) => {
-        console.log(e.target.value);
+        console.log(e.target.name);
     };
    
     return (
         <div className='container'><br /> <br />
             <input type="text" name ='ChannelName' placeholder='Channel Name' className='form-control' onChange={FormChange} />
+            <input type="text" name ='Subscriber' placeholder='Subscriber' className='form-control' onChange={FormChange} />
         </div>
     );
 }
