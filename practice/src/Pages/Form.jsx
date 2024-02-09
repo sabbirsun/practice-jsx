ccac2ec1d352db9102e6fefef6755b99eb90ccc0
sabbirsun <sabbirsun1920@gmail.com>
2024-02-09 22:34:52 +0600

    form table

--- practice/src/Pages/Form.jsx
+++ practice/src/Pages/Form.jsx
@@ -1,34 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function Form() {
-    const FormChange=(e)=>{
-        console.log(e.target.name)
-    }
-  return (
-    <div className='container'>
-        <select name="category" onChange={FormChange} className='form-control'>
-            <option value="Select">Select</option>
-            <option value="Select">PHP</option>
-            <option value="Select">JavaScript</option>
-            <option value="Select">Java</option>
-        </select>
+    const [state, setState] = useState({
+        ChannelName: "",
+        Subscriber: "",
+        category: "",
+        CreationDate: "",
+        About: "",
+        gender: "",
+        checkbox: true
+    });
 
-        <input type="date" name='CreationDate' onChange={FormChange} className='form-control'/>
+    const FormChange = (e) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        });
+    };
 
-        <textarea name="About" onChange={FormChange} placeholder='Tell me your about yourself' className='form-control'> </textarea>
+    const CheckBoxHandler = (e) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.checked
+        });
+    };
 
-            {/* Radio Button */}
+    const handleSubmit = () => {
+        console.log(state);
+        // Add your form submission logic here
+    };
+
+    return (
+        <div className='container'>
+            <input type="text" name='ChannelName' placeholder='Channel Name' className='form-control' onChange={FormChange} value={state.ChannelName} />
+            <input type="text" name='Subscriber' placeholder='Subscriber' className='form-control' onChange={FormChange} value={state.Subscriber} />
+            <select name="category" onChange={FormChange} className='form-control' value={state.category}>
+                <option value="Select">Select</option>
+                <option value="PHP">PHP</option>
+                <option value="JavaScript">JavaScript</option>
+                <option value="Java">Java</option>
+            </select>
+
+            <input type="date" name='CreationDate' onChange={FormChange} className='form-control' value={state.CreationDate} />
+
+            <textarea name="About" onChange={FormChange} placeholder='Tell me about yourself' className='form-control' value={state.About}> </textarea>
+
+            {/* Radio Buttons */}
 
             <div>
-                <input type="radio" name="gender" onChange={FormChange} id="male" />
+                <input type="radio" name="gender" onChange={FormChange} id="male" value="male" />
                 <label htmlFor="male">Male</label>
-                <input type="radio" name="gender" onChange={FormChange} id="female" />
+                <input type="radio" name="gender" onChange={FormChange} id="female" value="female" />
                 <label htmlFor="female">Female</label>
             </div>
 
-       
-    </div>
-  )
+            {/* Checkbox */}
+
+            <input type="checkbox" name='checkbox' onChange={CheckBoxHandler} checked={state.checkbox} />
+            <label htmlFor="checkbox">I agree & Continue</label>
+<br />
+            <button className='btn btn-secondary' onClick={handleSubmit}>
+                Submit
+            </button>
+        </div>
+    );
 }
 
-export default Form
+export default Form;
--- practice/src/Pages/Input.jsx
+++ practice/src/Pages/Input.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 
 function Input() {
-    const FormChange = (e) => {
-        console.log(e.target.value); // Use e.target.value to get the selected value
-    };
+    // const FormChange = (e) => {
+    //     console.log(e.target.value); // Use e.target.value to get the selected value
+    // };
    
     return (
         <div className='container'><br /><br />
-            <input type="text" name ='ChannelName' placeholder='Channel Name' className='form-control' onChange={FormChange} />
-            <input type="text" name ='Subscriber' placeholder='Subscriber' className='form-control' onChange={FormChange} />
-           <select name="category" onChange={FormChange} className='form-control'> {/* Use FormChange directly */}
-                <option value="Select">Select</option>
-                <option value="C-Programming">C-Programming</option>
-                <option value="C++">C++</option>
-                <option value="JavaScript">JavaScript</option>
-            </select>
+            
+          
         </div>
     );
 }
