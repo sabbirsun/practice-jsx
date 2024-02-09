e4dc3ef04a8fc29921ead3134a8c7c7c1018b755
sabbirsun <sabbirsun1920@gmail.com>
2024-02-09 21:01:01 +0600

    Select

--- practice/src/Pages/Input.jsx
+++ practice/src/Pages/Input.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 function Input() {
     const FormChange = (e) => {
-        console.log(e.target.name);
+        console.log(e.target.value); // Use e.target.value to get the selected value
     };
    
     return (
-        <div className='container'><br /> <br />
+        <div className='container'><br /><br />
             <input type="text" name ='ChannelName' placeholder='Channel Name' className='form-control' onChange={FormChange} />
             <input type="text" name ='Subscriber' placeholder='Subscriber' className='form-control' onChange={FormChange} />
+           <select name="category" onChange={FormChange} placeholder="category"> {/* Use FormChange directly */}
+                <option value="C-Programming">C-Programming</option>
+                <option value="C++">C++</option>
+                <option value="JavaScript">JavaScript</option>
+            </select>
         </div>
     );
 }
