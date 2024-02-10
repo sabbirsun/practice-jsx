4a21561ad75a8bedf49189ff8f35cab5491fdcf5
sabbirsun <sabbirsun1920@gmail.com>
2024-02-10 19:34:51 +0600

    expressions

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,25 +1,26 @@
 import './App.css';
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Contact from './Pages/Contact';
-import Feedback from './Pages/Feedback';
-import Fragment from './Pages/Fragment';
-import React from 'react';
-import Clipboardoncopy from './Pages/Clipboardoncopy';
-import Focus from './Pages/Focus';
-import Keyboard from './Pages/Keyboard';
-import Fiter from './Pages/Fiter';
-import WelComeAdmin from './Pages/WelComeAdmin'
-import Style from './Pages/Style';
-import ReactBootstrap from './Pages/ReactBootstrap';
-import Input from './Pages/Input';
+// import Home from './Pages/Home';
+// import About from './Pages/About';
+// import Contact from './Pages/Contact';
+// import Feedback from './Pages/Feedback';
+// import Fragment from './Pages/Fragment';
+// import React from 'react';
+// import Clipboardoncopy from './Pages/Clipboardoncopy';
+// import Focus from './Pages/Focus';
+// import Keyboard from './Pages/Keyboard';
+// import Fiter from './Pages/Fiter';
+// import WelComeAdmin from './Pages/WelComeAdmin'
+// import Style from './Pages/Style';
+// import ReactBootstrap from './Pages/ReactBootstrap';
+// import Input from './Pages/Input';
 import Form from './Pages/Form';
 import Controlled from './Pages/Controlled';
+import Expressions from './Pages/Expressions';
 
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home />
       <About />
       <Contact />
       <Feedback />
@@ -31,9 +32,10 @@ function App() {
       <WelComeAdmin />
       <Style />
       <ReactBootstrap />
-      <Input />
+      <Input /> */}
       <Form />
       <Controlled />
+      <Expressions />
     </>
   );
 }
---
+++ practice/src/Pages/Expressions.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Expressions() {
+    const obj = {
+        name: "Sabbir",
+        Roll: 109,
+        Prof: "Programmer"
+    };
+
+    return (
+        <div>
+            <h4>I am a Programmer {obj.Prof}</h4>
+        </div>
+    );
+}
+
+export default Expressions;
