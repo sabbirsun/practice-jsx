db5090a9673ff1e4fc4b39bc802a39c99a67f0d9
sabbirsun <sabbirsun1920@gmail.com>
2024-02-12 23:09:12 +0600

    carousel

--- practice/src/Pages/BlogWebsite/Carousel.jsx
+++ practice/src/Pages/BlogWebsite/Carousel.jsx
@@ -1,66 +1,63 @@
-import React from 'react'
-import "./BlogCss/Blog.css"
+import React from 'react';
+import "./BlogCss/Blog.css";
 
 function Carousel() {
   return (
     <div>
-        <div id="carouselExampleFade" class="carousel slide carousel-fade container mt-5" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-        <div className='row'>
-            <div className='col-md-6'>
-                 <img src="img/natural.jpg" class="d-block w-100" alt="..." />
+      <div id="carouselExampleFade" className="carousel slide carousel-fade container mt-5" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <div className='row'>
+              <div className='col-md-6'>
+                <img src="img/natural.jpg" className="d-block mh-100" style={{ width: "600px", height: "200px" }} alt="..." />
+              </div>
+              <div className='col-md-6'>
+                <p className='ShortTitle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quisquam, esse expedita nostrum aperiam dolore repellat necessitatibus dolor earum alias.</p>
+                <h3 className='LongTitle'>Live with Sabbir Sun</h3>
+                <p className='ShortTitle'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis doloribus natus dolor asperiores non est!</p>
+                <p className='ChannelName'>Sabbir sun Programmer</p>
+                <p className='ShortTitle'>12 February 2024</p>
+              </div>
             </div>
-            <div className='col-md-6'>
-                  <p className='ShortTitle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quisquam, esse expedita nostrum aperiam dolore repellat necessitatibus dolor earum alias.</p>
-                  <h3 className='LongTitle'>Live with Sabbir Sun</h3>
-                  <p className='ShortTitle'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis doloribus natus dolor asperiores non est!</p>
-                  <p className='ChannelName'>Sabbir sun Programmer</p>
-                  <p className='ShortTitle'>12 February 2024</p>
+          </div>
+          <div className="carousel-item">
+            <div className='row'>
+              <div className='col-md-6'>
+                <img src="img/road.jpg" className="d-block mh-100" style={{ width: "600px", height: "200px" }} alt="..." />
+              </div>
+              <div className='col-md-6'>
+                <p className='ShortTitle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quisquam, esse expedita nostrum aperiam dolore repellat necessitatibus dolor earum alias.</p>
+                <h3 className='LongTitle'>Live with Sabbir Sun</h3>
+                <p className='ShortTitle'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis doloribus natus dolor asperiores non est!</p>
+                <p className='ChannelName'>JavaScript</p>
+                <p className='ShortTitle'>12 February 2024</p>
+              </div>
             </div>
+          </div>
+          <div className="carousel-item">
+            <div className='row'>
+              <div className='col-md-6'>
+                <img src="img/tree.jpg" className="d-block mh-100" style={{ width: "600px", height: "200px" }} alt="..." />
+              </div>
+              <div className='col-md-6'>
+                <p className='ShortTitle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quisquam, esse expedita nostrum aperiam dolore repellat necessitatibus dolor earum alias.</p>
+                <h3 className='LongTitle'>Live with Sabbir Sun</h3>
+                <p className='ShortTitle'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis doloribus natus dolor asperiores non est!</p>
+                <p className='ChannelName'>Python</p>
+                <p className='ShortTitle'>12 February 2024</p>
+              </div>
+            </div>
+          </div>
         </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon"></span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+          <span className="carousel-control-next-icon"></span>
+        </button>
+      </div>
     </div>
-    <div class="carousel-item">
-        <div className='row'>
-            <div className='col-md-6'>
-                 <img src="img/road.jpg" class="d-block w-100" alt="..." />
-            </div>
-            <div className='col-md-6'>
-                  <p className='ShortTitle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quisquam, esse expedita nostrum aperiam dolore repellat necessitatibus dolor earum alias.</p>
-                  <h3 className='LongTitle'>JavaScript</h3>
-                  <p className='ShortTitle'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis doloribus natus dolor asperiores non est!</p>
-                  <p className='ChannelName'>Sabbir sun Programmer</p>
-                  <p className='ShortTitle'>12 February 2024</p>
-            </div>
-        </div>
-    </div>
-    <div class="carousel-item">
-        <div className='row'>
-            <div className='col-md-6'>
-                 <img src="img/tree.jpg" class="d-block w-100" alt="..." />
-            </div>
-            <div className='col-md-6'>
-                  <p className='ShortTitle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quisquam, esse expedita nostrum aperiam dolore repellat necessitatibus dolor earum alias.</p>
-                  <h3 className='LongTitle'>Python</h3>
-                  <p className='ShortTitle'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis doloribus natus dolor asperiores non est!</p>
-                  <p className='ChannelName'>Sabbir sun Programmer</p>
-                  <p className='ShortTitle'>12 February 2024</p>
-            </div>
-        </div>
-    </div>
-  
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-    
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-    <span class="carousel-control-next-icon"></span>
- 
-  </button>
-</div>
-    </div>
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
